ef14167bfcfb2ad50e2674346a3124dcd24ced28
moongi choe <cmg3172@naver.com>
2020-10-19 17:09:35 +0900

    cp vscode

--- Shell01/ex04/MAC.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ifconfig -a link | grep ether | tr -d '[:blank:]' | sed 's/ether//g'
--- Shell01/ex07/r_dwssap.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat /etc/passwd | sed -n '/^#/!p' | sed -n 'n;p' | cut -f 1 -d ':' |  rev | sort -r | sed -n "$FT_LINE1,$FT_LINE2 p" | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/./' | tr -d '\n'
